efc127e2522358a79743b48d57a835666ab0512b
Zhaoyan Xie <zhaoyanxie@Zhaoyans-MacBook-Pro.local>
2018-05-30 09:54:29 +0800

    Three cards added

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Recipe from './Recipe';
+import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
@@ -8,13 +9,13 @@ class RecipeApp extends Component {
     return (
       <div className="RecipeApp">
         <Navbar />
-        <Recipe 
+        {/* <Recipe 
           title="Pasta"
           ingredients={["pasta", "bolognese", "basil"]}
           img="pasta.jpg"
           instructions="Follow These Steps. Boil water in a large pot. To make sure pasta doesn't stick together, use at least 4 quarts of water for every pound of noodles. Salt the water with at least a tablespoon—more is fine. The salty water adds flavor to the pasta. Add pasta. Stir the pasta. Test the pasta by tasting it. Drain the pasta."
-        />
-        
+        /> */}
+        <RecipeList />
       </div>
     );
   }
