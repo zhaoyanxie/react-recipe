9362c2af8e9abe05c6f69e99f468debddd13f750
Zhaoyan Xie <zhaoyanxie@Zhaoyans-MacBook-Pro.local>
2018-05-30 09:15:33 +0800

    Added navbar

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import Recipe from './Recipe';
 import './RecipeApp.css';
 
@@ -6,13 +7,14 @@ class RecipeApp extends Component {
   render() {
     return (
       <div className="RecipeApp">
+        <Navbar />
         <Recipe 
           title="Pasta"
           ingredients={["pasta", "bolognese", "basil"]}
           img="pasta.jpg"
           instructions="Follow These Steps. Boil water in a large pot. To make sure pasta doesn't stick together, use at least 4 quarts of water for every pound of noodles. Salt the water with at least a tablespoon—more is fine. The salty water adds flavor to the pasta. Add pasta. Stir the pasta. Test the pasta by tasting it. Drain the pasta."
-          
         />
+        
       </div>
     );
   }
